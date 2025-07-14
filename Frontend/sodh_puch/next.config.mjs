2459429b2d4ba85ade89a346551d49d391ea25ad
Anish <111975682+AnishBharati@users.noreply.github.com>
2025-07-14 08:22:08 +0545

    Update next.config.mjs

--- Frontend/sodh_puch/next.config.mjs
+++ Frontend/sodh_puch/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static HTML export
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
